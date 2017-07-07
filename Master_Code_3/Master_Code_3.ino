87109ec289610682c71340660468874a79651613
capuanomat <capuanomat@gmail.com>
2017-07-06 22:33:32 -0400

    Masted Code 3 is latest working one 06/07

--- Master_Code_3/Master_Code_3.ino
+++ Master_Code_3/Master_Code_3.ino
@@ -14,18 +14,21 @@ double unfilteredPitch, pitch, desiredPitch, errorPitch, lastPitch;
 double unfilteredRoll, roll;
 
 /** ADJUSTABLE: Setting a default pitch angle, sampling time in milliseconds, filter value, and  **/
-double defaultAngle = - 20;
+double defaultAngle = - 10;
 int SampleTime = 4;
-float filterVal = 0.85;
+float filterVal = 0.99;
 
 /** The PID gains and PID terms **/
-float kp, ki, kd;
+double kp, ki, kd;
 double PTerm, ITerm, DTerm, DTermUnfiltered;
+double ITermMin = 0;
+double ITermMax = 2;
 double sumPID;
 
 /** To read serial monitor input angles **/
 char readMonitor[3];  //CHECKON: changed this from "test" which was a keyword
 
+/** Output, as well as it's minimum and maximum possible values to avoid integral wind up/overshoot **/
 double Out;
 
 void setup() {
@@ -69,19 +72,37 @@ void loop() {
     // This will involve editing how values are read in so you can input both roll and pitch
   
     /** Reading the PID gains from potentiometers on Analog ports 0, 1, and 2. Casting them to reasonable ranges **/
-    kp = map(analogRead(A0), 0, 1023, 0, 3000)   / 100000.0;
-    ki = map(analogRead(A1), 0, 1023, 0, 90)     / 1000000.0;
-    kd = map(analogRead(A2), 0, 1023, 0, 100000) / 1000000.0;
-
+    double pot1 = analogRead(A0);
+    double pot2 = analogRead(A1);
+    double pot3 = analogRead(A2);
+    double potMin = 0;
+    double potMax = 1023;
+    double pMin = 0;
+    double iMin = 0;
+    double dMin = 0;
+    double pMax = 0.01;
+    double iMax = 0.0002;
+    double dMax = 0.005;    
+    kp = (((pot1 - potMin) * (pMax - pMin)) / (potMax - potMin)) + pMin;
+    ki = (((pot2 - potMin) * (iMax - iMin)) / (potMax - potMin)) + iMin;
+    kd = (((pot3 - potMin) * (dMax - dMin)) / (potMax - potMin)) + dMin;
+        
     /** Calculating the time since last reading **/
     unsigned long now = millis();
     int timeChange = (now - lastTime);
 
     /** Computing the PID terms if timeChange is large enough **/
     if(timeChange >= SampleTime){
-      /** P and I **/
+      /** P **/
       PTerm  = kp * errorPitch; // Proportional to instantaneous error in pitch
+      
+      /** I and clamping to prevent Integral wind up **/
       ITerm += ki * errorPitch; // Summed error in pitch over time
+      if (ITerm > ITermMax) {
+        ITerm = ITermMax;
+      } else if (ITerm < ITermMin) {
+        ITerm = ITermMin;
+      }
 
       /** D **/
       double dpitch = (pitch - lastPitch);
@@ -90,7 +111,7 @@ void loop() {
       
       /** Compute sum of P, I, and D terms **/
       sumPID = PTerm + ITerm + DTerm; 
- 
+
       /** Remember some variables for next time **/
       lastPitch = pitch;
       lastTime = now;
@@ -99,55 +120,29 @@ void loop() {
     /** Error from -87 to 43 is mapped from -1 to 1**/
     //Mapped = map(Mapped, -87, 44, 1000, 1370);  // NOTE: Mapped was changed to sumPID
     //Mapped = map(Mapped, -87, 43, -1, 1);
-    if (sumPID > 1) {
-      sumPID = 1;
-    } else if (sumPID < -1) {
-      sumPID = -1;
+    double controlOutMin = 0;
+    double controlOutMax = 1;
+    double mixMin = 1150;
+    double mixMax = 2000;
+    if (sumPID > controlOutMax) {
+      sumPID = controlOutMax;
+    } else if (sumPID < controlOutMin) {
+      sumPID = controlOutMin;
     }
 
     /** Input from -1 to 1 mapped to 1100 to 2000 **/
-    Out = ((sumPID + 1) * (1400 - 1100)/ (1 + 1)) + 1000;
-    //Out = map(Mapped, 1-, 1, 1100, 2000);
+    Out = (((sumPID - controlOutMin) * (mixMax - mixMin)) / (controlOutMax - controlOutMin)) + mixMin;
   }
   
   esc.write(Out);
-  
-  /** ---------- PRINTING SECTION (comment out what you don't need) ----------**/
-  /** Unfiltered, filtered, desired pitch, and pitch error **/
-  //Serial.print(" Unfiltered Pitch = ");
-  //Serial.println(unfilteredPitch);
-  //Serial.print(" Pitch = ");
-  //Serial.print(pitch);
-  //Serial.print(" Pitch_d = ");
-  //Serial.println(desiredPitch);
-  Serial.print(" Pitch_error = ");
-  Serial.println(errorPitch);
-  
-  /** Gains and PID Terms **/
-  //Serial.print("\t kp: ");
-  //Serial.print(kp, 3);
-  //Serial.print(" ki: ");
-  //Serial.print(ki, 8);
-  //Serial.print(" kd: ");
-  //Serial.print(kd, 5);
-  //Serial.print("\t P Term = ");
-  //Serial.print(PTerm);
-  //Serial.print(" I Term = ");
-  //Serial.print(ITerm);
-  //Serial.print(" D Term = ");
-  //Serial.print(DTerm);
-
-  /** Output after mapping...?? **/
-  //Serial.print(" Sum PID Terms = ");
-  //Serial.print(sumPID);
-  //Serial.print(" Out = ");
-  //Serial.println(Out);
+  printGains();
+  //printPID();
+  //printPitch();
+  //printError();
+  //printOut();
 }
 
-int Mixer(int mapped) {
-  int Out = map(mapped, -1, 1, 1100, 2000);
-  return Out;
-}
+//double linInterp(double input, 
 
 double smooth(double data, float filterVal, double lastData){
   /** Makes sure parameters are within range **/
@@ -161,6 +156,46 @@ double smooth(double data, float filterVal, double lastData){
   //TODO: Read up on where this equation came from (makes sense though)
   double smoothedVal = (data * (1 - filterVal)) + (lastData * filterVal);
   return smoothedVal;
+}
+
+
+void printPitch() {
+  Serial.print(" Unfiltered Pitch = ");
+  Serial.println(unfilteredPitch);
+  Serial.print(" Pitch_d = ");
+  Serial.println(desiredPitch);
+  Serial.print(" Pitch = ");
+  Serial.print(pitch);
+}
+
+void printError() {
+  Serial.print(" Pitch_error = ");
+  Serial.println(errorPitch);
+}
+
+void printGains() {
+  Serial.print("\t kp: ");
+  Serial.print(kp, 6);
+  Serial.print(" ki: ");
+  Serial.print(ki, 6);
+  Serial.print(" kd: ");
+  Serial.println(kd, 6);
+}
+
+void printPID() {
+  Serial.print("\t P-Term = ");
+  Serial.print(PTerm);
+  Serial.print(" I-Term = ");
+  Serial.print(ITerm);
+  Serial.print(" D-Term = ");
+  Serial.print(DTerm);
+  Serial.print(" sumPID = ");
+  Serial.print(sumPID);
+}
+
+void printOut() {
+  Serial.print(" Out = ");
+  Serial.println(Out);
 }
 
 /** Initial attempt at filtering, kept here for reference, originally went right after unfiltered pitch reading **/
